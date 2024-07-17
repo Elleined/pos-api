b1bc55fbfaa515eee27970bee575b0515ded5d3a
Elleined <demadegu@gmail.com>
2024-07-17 22:33:15 +0800

    Added eureka discovery client

--- src/main/java/com/elleined/pos_api/PosApiApplication.java
+++ src/main/java/com/elleined/pos_api/PosApiApplication.java
@@ -2,12 +2,14 @@ package com.elleined.pos_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class PosApiApplication {
