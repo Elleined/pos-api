87426a3df5d0f4143d22259afe7ce728cd278540
Elleined <demadegu@gmail.com>
2024-08-12 19:40:26 +0800

    added module of store

--- src/main/java/com/elleined/pos_api/controller/store/StoreController.java
+++ src/main/java/com/elleined/pos_api/controller/store/StoreController.java
@@ -5,10 +5,7 @@ import com.elleined.pos_api.mapper.store.StoreMapper;
 import com.elleined.pos_api.model.store.Store;
 import com.elleined.pos_api.service.store.StoreService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/stores")
@@ -18,17 +15,17 @@ public class StoreController {
     private final StoreMapper storeMapper;
 
     @PostMapping
-    public StoreDTO save(String name,
-                         String address,
-                         String contactNumber) {
+    public StoreDTO save(@RequestParam("name") String name,
+                         @RequestParam("address") String address,
+                         @RequestParam("contactNumber") String contactNumber) {
 
         Store store = storeService.save(name, address, contactNumber);
 
         return storeMapper.toDTO(store);
     }
 
-    @GetMapping
-    public StoreDTO getById(int id) {
+    @GetMapping("/{id}")
+    public StoreDTO getById(@PathVariable("id") int id) {
         Store store = storeService.getById(id);
         return storeMapper.toDTO(store);
     }
