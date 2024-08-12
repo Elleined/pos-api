349a80a22137e9049b05106697aec418acef4ae7
Elleined <demadegu@gmail.com>
2024-08-12 18:23:48 +0800

    Thymeleaf template is now tested

--- src/main/java/com/elleined/pos_api/service/receipt/ReceiptGenerator.java
+++ src/main/java/com/elleined/pos_api/service/receipt/ReceiptGenerator.java
@@ -8,11 +8,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -20,16 +25,24 @@ import java.io.IOException;
 @Transactional
 @RequiredArgsConstructor
 public class ReceiptGenerator {
-    private final static String RESOURCES_BASE_URI = "src/main/resources/pdf";
-    private final static String TARGET = "src/main/resources";
+    private final TemplateEngine templateEngine;
 
-    public void generate() throws IOException {
-        ConverterProperties converterProperties = new ConverterProperties();
-        converterProperties.setBaseUri(RESOURCES_BASE_URI);
+    public void generate() {
+        Context context = new Context();
+        context.setVariable("name", "Denielle");
+        context.setVariable("names", List.of("name1", "name2", "name3"));
 
-        final String html = STR."\{RESOURCES_BASE_URI}/html/receipt.html";
-        final String pdf = STR."\{TARGET}/receipt.pdf";
-
-        HtmlConverter.convertToPdf(new FileInputStream(html), new FileOutputStream(pdf), converterProperties);
+        String html = templateEngine.process("sample.html", context);
+        System.out.println(html);
     }
+
+//    public void generate() throws IOException {
+//        ConverterProperties converterProperties = new ConverterProperties();
+//        converterProperties.setBaseUri(RESOURCES_BASE_URI);
+//
+//        final String html = STR."\{RESOURCES_BASE_URI}/html/receipt.html";
+//        final String pdf = STR."\{TARGET}/receipt.pdf";
+//
+//        HtmlConverter.convertToPdf(new FileInputStream(html), new FileOutputStream(pdf), converterProperties);
+//    }
 }
